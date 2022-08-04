939695d3cb7667f7a7458acdc2d5913c070ec7ba
Limgahyeon <109576587+Limgahyeon@users.noreply.github.com>
2022-08-04 17:10:20 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import streamlit as st
-view =[300,200,100,500,600]
+ import streamlit as st
+ view = [300, 200, 100, 500]
 
-st.write('raw data')
-
-
-#agree = st.checkbox('I agree')
+ show_raw = st.checkbox{'show raw data'}
+ if show_raw == True
+    st.write('#raw data')
+    view
+st.write('#table')
+st.table(View)
+st.write('#bar graph')
+st.bar_chart(view)
